64eff73f8378bfc9551ab90a85197c35afc7ad1b
Ray Linder <raylinder@affinitylogic.com>
2024-09-16 12:28:29 -0500

    fixed issue with swagger.

--- raylinder.API/Program.cs
+++ raylinder.API/Program.cs
@@ -12,14 +12,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 //}
 
-app.UseSwagger();
+//app.UseSwagger();
 //if (app.Environment.IsDevelopment())
 //{
-app.UseSwaggerUI();
+//    app.UseSwaggerUI();
 //}
 //if (!app.Environment.IsDevelopment())
 //{
