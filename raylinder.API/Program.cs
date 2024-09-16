13d207dac81ca5c36a2d08535659ccf368b44df2
Ray Linder <raylinder@affinitylogic.com>
2024-09-16 11:24:03 -0500

    enabling swagger UI to always be available.

--- raylinder.API/Program.cs
+++ raylinder.API/Program.cs
@@ -17,18 +17,18 @@ var app = builder.Build();
 //}
 
 app.UseSwagger();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwaggerUI();
-}
-if (!app.Environment.IsDevelopment())
-{
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
-    });
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwaggerUI();
+//}
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseSwaggerUI(options =>
+//    {
+//        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+//        options.RoutePrefix = string.Empty;
+//    });
+//}
 
 app.UseAuthorization();
 
